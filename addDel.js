0266ce5c1cbcc19e06bb6bda2786730cb4c5f06d
haqjahirul123 <56026695+haqjahirul123@users.noreply.github.com>
2020-01-30 22:25:12 +0100

    Add files via upload

--- addDel.js
+++ addDel.js
@@ -6,24 +6,59 @@ document.getElementById('btnDelete').addEventListener('click', deleteApiData);
 //document.getElementById('book-form').addEventListener('submit',postApiData);
 
 
+
  function addApiData() {
+    //
+
+
+    //
     let $output=$('#output');
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
 
     //let key='VkrYv';
     let key='VkrYv';
-    URL='https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key + '&op=insert&title=' + title + '&author=' + author;
-     fetch(URL)
+    let URL='https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key + '&op=insert&title=' + title + '&author=' + author;
+    console.log('url', URL);
+    
+    fetch(URL , {
+        method: 'GET'
+     
+      })
     .then(function(response) {
-        return response.json();
+        //console.log(response.status)
+
+        
+        if (response.status >= 200 && response.status <= 299) {
+            
+            return response.json();
+          } else {
+            throw Error(response.statusText);
+          }
+        //return response.json();
     })
     .then(function(data) {
-         console.log(data);
-        // document.getElementById('output').innerHTML=data;
+        counter= 0;
+        if(data.status !== 'success' && counter < 5){  //if the respnse is error or in other words notSuccsess call it self again
+            console.log('failed trying again' + counter) // with incremented counter and if the counter is above 5 (comment below)
+            counter++;
+      
+            addApiData( );
+        }else if(data.status !== 'success' && counter < 5){ // do not call its self again and tell the user it failed
+            
+                console.log("failed to add books 5/5 tries. Try again!")
+        }else{ // if the book is added successfully at 5 times max tells the user it did succeeeeed
+            
+            console.log('Book Added on try ' + counter +'/5 tries' )
+           
+           console.log(data);
+         
+        }
+      
     })
     .catch(function(error) {
-        console.log(error);
+        
+        console.log('error');
         
     });
 
@@ -47,32 +82,39 @@ function viewApiData(){
     
     fetch(URL) 
     .then(function(response) {
-        return response.json();
+        if (response.status >= 200 && response.status <= 299) {
+            return response.json();
+          } else {
+            throw Error(response.statusText);
+          }
     })
     .then((books)=>{
         console.log(books);
         let result= '';
-        if(books.status === 'success') {
+        counter=0;
+
+        if(books.status === 'success' && counter < 5) {
             for(let i=0;i<books.data.length;i++) {
                 result += `
-                    <div>
+                    <div class='result'>
                     <h3>${books.data[i].title}</h3>
                     <p></b>${books.data[i].author}</p>
                     <p></b>${books.data[i].id}</p>
                     </div> `     
                     ;                  
             }
-            //console.log(jjjjj);
+            console.log('Books shows properly');
             document.getElementById('output').innerHTML=result;
-        }
-        else {
-            console.error(error);
             
         }
-
+        else{ 
+            counter++;
+            viewApiData(counter++);
+           console.log('Books not showing properly  within' + counter +'/5 tries' ) ;
+        }
     })
     .catch(function(error) {
-        console.log(error);
+        console.error(error);
         
     }); 
     
@@ -91,39 +133,20 @@ function deleteApiData(){
    
     })
     .then(function(response) {
-        return response.json();
+        if (response.status >= 200 && response.status <= 299) {
+            return response.json();
+          } else {
+            throw Error(response.statusText);
+          }
+        //return response.json();
     })
     .then(function(data) {
         console.log(data);
        // document.getElementById('output').innerHTML=data;
    })
    .catch(function(error) {
-        console.log("error is:", error);
+        console.error( error);
         
     });
 
 }
-/////////////////////////
-
-// fetch('https://jsonplaceholder.typicode.com/posts')
-//     .then((res)=>res.json())
-//     .then((data)=>{
-//         let result= '<h2>Books Details</h2>';
-//         data.forEach(function(post){
-//             result += `
-//             <div>
-//             <h3>Title:${post.title}</h3>;
-//             <p><b>Body:</b>${post.body}</p>;
-//             </div> `
-         
-//             ;   
-            
-
-//         }); 
-//                 //console.log(jjjjj);
-//         document.getElementById('output').innerHTML=result;
-
-//     })
-//     .catch(function(error) {
-//         console.log('error!!Recheck please');
-//       });
